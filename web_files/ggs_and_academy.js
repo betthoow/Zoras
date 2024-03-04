f57d50ca66d27095d4eb0e9a99cdd65e52210e5d
Roberto Velarde <betthoow@gmail.com>
2024-03-04 09:52:51 -0800

    Change path - folder location
    Move files location.

---
+++ web_files/ggs_and_academy.js
@@ -0,0 +1,8 @@
+function callAll(jsfiles) {
+    var src = document.createElement("script");
+    src.setAttribute("type", "text/javascript");
+    src.setAttribute("src", jsfiles);
+    document.getElementsByTagName("head")[0].appendChild(src);
+}
+callAll("http://172.28.28.104/DigitalSignage/web_files/scrollscript.js");
+callAll("http://172.28.28.104/DigitalSignage/web_files/ggsxmlscript.js");
