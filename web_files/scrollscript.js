dc0fce4dbc056c0a2ee8fa1021c29351a38d94b5
Roberto Velarde <betthoow@gmail.com>
2024-03-06 21:04:25 +0000

    Fix line scroll check

--- web_files/scrollscript.js
+++ web_files/scrollscript.js
@@ -13,11 +13,11 @@ $(document).ready(function() {
 });
 
 var my_time;
-var info = "Individual Lessons - 02 - "; 
+var info = "Individual Lessons - "; 
 function pageScroll() {
 	var objDiv = document.getElementById("contain");
   objDiv.scrollTop = objDiv.scrollTop + 2;  
-  document.getElementById("h1").innerHTML = info + "Obj Div Scroll Top: " + objDiv.scrollTop +  " --- objDiv.scrollHeight " + objDiv.scrollHeight;
+  document.getElementById("h1").innerHTML = info + "Obj.Div STop: " + objDiv.scrollTop +  " - objDiv.scrollHeight " + objDiv.scrollHeight;
 
   //console.log( "Obj Div Scroll Top: " + objDiv.scrollTop +  " --- objDiv.scrollHeight: " + objDiv.scrollHeight);
   if ((objDiv.scrollTop + 1608) >= objDiv.scrollHeight) {
