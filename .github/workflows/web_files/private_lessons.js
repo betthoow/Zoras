01f6fde8a19e93eefe395d44b74ee4ef19c527ed
Roberto Velarde <betthoow@gmail.com>
2024-03-07 00:09:14 +0000

    Delete wrong files from github folder

--- .github/workflows/web_files/private_lessons.js
+++
@@ -1,8 +0,0 @@
-function callAll(jsfiles) {
-    var src = document.createElement("script");
-    src.setAttribute("type", "text/javascript");
-    src.setAttribute("src", jsfiles);
-    document.getElementsByTagName("head")[0].appendChild(src);
-}
-callAll("./web_files/scrollscript.js");
-callAll("./web_files/plxmlscript.js");
